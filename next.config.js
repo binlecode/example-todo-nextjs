31e867fbdd259b9c5c63ba1ce3142d76e50ac102
Bin Le <bin.le.code@gmail.com>
2023-08-08 14:34:49 -0400

    fix gh-pages config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+let assetPrefix = "";
+let basePath = "/";
+
+// todo: get this from env var
+deployToGhPages = True;
+
+if (deployToGhPages) {
+  const repo = "example-todo-nextjs";
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
-  output: "standalone",
-  distDir: "dist"
+  // output: "standalone",
+  // enable statix export for gh-pages hosting
+  output: "export",
+  // set output dir to `dist`
+  distDir: "dist",
+
+  // asset prefix to support gh-pages repo url path
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
